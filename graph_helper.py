3acc7bc68040ed21526f03f12826f5a45375f0a5
Anthony qin <qin2500@gmail.com>
2023-12-26 17:54:49 -0600

    Finally have Node generation working correctly
    -the dataframe.apply function being used on the chunks df was using the wrong axis.

--- graph_helper.py
+++ graph_helper.py
@@ -7,13 +7,22 @@ from openai import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 
-class graph_helper:
-    chat = None
 
+def get_concepts(prompt:pd.DataFrame):
+    print("POOOOOOOO")
+    prompt = prompt[0]
+    assert isinstance(prompt, str), "prompt must be a string"
+    print(prompt)
+    load_dotenv()
+    chat = ChatOpenAI(temperature=0, openai_api_key = os.getenv("OPENAI_API_KEY"),  model="gpt-3.5-turbo")
     SYS_PROMPT = (
             "You are a network graph maker who extracts terms and their relations from a given context. "
-            "You are provided with a context chunk (delimited by ```) Your task is to extract the ontology "
+            
+        )
+    
+    USER_PROMPT = ("You are provided with a context chunk (delimited by ```) Your task is to extract the ontology "
             "of terms mentioned in the given context. These terms should represent the key concepts as per the context. \n"
+            "It is very important that you perform your task only on the text provided within the context chunk and nothing else."
             "Thought 1: While traversing through each sentence, Think about the key terms mentioned in it.\n"
                 "\tTerms may include object, entity, location, organization, person, \n"
                 "\tcondition, acronym, documents, service, concept, etc.\n"
@@ -31,38 +40,29 @@ class graph_helper:
             '       "edge": "relationship between the two concepts, node_1 and node_2 in one or two sentences"\n'
             "   }, {...}\n"
             "]"
-        )
+            f"context: ```{prompt}``` \n\n ")
     messages = [
-            SystemMessage(
-                content=SYS_PROMPT
-            ),
-            None
-        ]
-    def __init__ (self):
-        self.chat = ChatOpenAI(temperature=0, openai_api_key = os.getenv("OPENAI_API_KEY"),  model="gpt-3.5-turbo")
+        SystemMessage(
+            content=SYS_PROMPT
+        ),
+        HumanMessage(
+            content=USER_PROMPT
+        )
+    ]
+   
+    response = chat(messages).content
+    try:
+        result = json.loads(response)
+    except:
+        print("\n\nERROR ### Here is the buggy response: ", response, "\n\n")
+        result = None
+    return result
+    
 
-    def get_concepts(self,prompt: str):
-        load_dotenv()
-        
-        USER_PROMPT = f"context: ```{prompt}``` \n\n "
-        
-        self.messages[1] = HumanMessage(
-                content=USER_PROMPT
-            )
-        
-        response = self.chat(self.messages).content
-        try:
-            result = json.loads(response)
-        except:
-            print("\n\nERROR ### Here is the buggy response: ", response, "\n\n")
-            result = None
-        return result
-        
+def formatDF(nodes_list: pd.DataFrame) -> pd.DataFrame:
+    graph_dataframe = pd.DataFrame(nodes_list).replace(" ", np.nan)
+    graph_dataframe = graph_dataframe.dropna(subset=["node_1", "node_2"])
+    graph_dataframe["node_1"] = graph_dataframe["node_1"].apply(lambda x: x.lower())
+    graph_dataframe["node_2"] = graph_dataframe["node_2"].apply(lambda x: x.lower())
 
-    def formatDF(nodes_list: pd.DataFrame) -> pd.DataFrame:
-        graph_dataframe = pd.DataFrame(nodes_list).replace(" ", np.nan)
-        graph_dataframe = graph_dataframe.dropna(subset=["node_1", "node_2"])
-        graph_dataframe["node_1"] = graph_dataframe["node_1"].apply(lambda x: x.lower())
-        graph_dataframe["node_2"] = graph_dataframe["node_2"].apply(lambda x: x.lower())
-
-        return graph_dataframe
+    return graph_dataframe
